e8573ea67cbd085c4acc13388e563550ea6d3c70
umair <20048189@mydbs.ie>
2024-09-27 07:45:08 +0000

    new changes

--- euler.js
+++ euler.js
@@ -1,1 +1,14 @@
 /*if we list all the natural numbers below 10 that are multiple of 3 and 5 we get 
+3, 5, 6, and 9.
+the sum of these multiple is 23
+find the sum of all the multipes of 3 or 5 below 1000. */
+
+sum = 0;
+for (let i = 1; i<1000; i++){
+    if (i % 3 == 0 || i % 5 == 0){
+        sum +=i;
+    }
+}
+alert (sum);
+
+
