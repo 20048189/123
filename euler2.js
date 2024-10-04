ce59a65323bc3e979a3a8d5da3b2a9c96cba20e6
umair <20048189@mydbs.ie>
2024-10-04 07:16:11 +0000

    changes

--- euler2.js
+++ euler2.js
@@ -14,6 +14,24 @@ function GetValues()
     num1 = document.getElementById("num1").value;
     num2 = document.getElementById("num2").value;
     limit = document.getElementById("limit").value;
-    <button onclick ="sum(num1,num2,limit)">check sum</button>
+    document.getElementById("num1").style.display="none";
+    document.getElementById("num2").style.display="none";
+    document.getElementById("limit").style.display="none";
+    document.getElementById("sum").style.display="none";
+    document.getElementById("label1").style.display="none";
+    document.getElementById("label2").style.display="none";
+    document.getElementById("label3").style.display="none";
+ 
     
+}
+
+ function Show()
+{
+    document.getElementById("num1").style.display="block";
+    document.getElementById("num2").style.display="block";
+    document.getElementById("limit").style.display="block";
+    document.getElementById("sum").style.display="block";
+    document.getElementById("label1").style.display="block";
+    document.getElementById("label2").style.display="block";
+    document.getElementById("label3").style.display="block";
 }
