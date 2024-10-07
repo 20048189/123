b96980a42142930d6a25b8542ee07a6be8c1678d
umair <20048189@mydbs.ie>
2024-10-07 10:41:51 +0000

    basket

---
+++ basket.js
@@ -0,0 +1,33 @@
+/*
+Create a function that takes two objects,
+basket and prices as parameters
+where basket is a mapping from products (string) to quantities (number)
+and prices is a mapping from products(String) to prices (number)
+and returns the total cost of the basket
+*/
+
+
+function calculateTotalCost(basket, prices) {
+    let totalcost = 0;
+
+    for (let product in basket) {
+        if (prices.hasOwnProperty(product)) {
+            totalCost += basket[product] * prices[product];
+        }
+    }
+
+    return totalCost;
+}
+const basket = {
+    "apple": 3,
+    "banana": 2,
+    "orange": 1
+};
+
+const prices = {
+    "apple": 1.5,
+    "banana": 0.5,
+    "orange": 0.75
+};
+
+alert(calculateTotalCost(basket, prices));
